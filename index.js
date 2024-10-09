71ec2651ab1626d0bdd664f21d304c385dfc21f7
Sebastien <fenelonsebastien@outlook.com>
2024-10-09 00:51:26 -0400

    review strings completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code in this file!
+const currentUser = "James"
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, + ${currentUser.toUpperCase()}!`
+
+const shortGreeting = `Welcome, ${currentUser[0]}!`
+
+
+ 
+
